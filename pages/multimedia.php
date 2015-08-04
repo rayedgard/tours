36be735b5b785719c74b509a3631a7581d348680
rayedgard <rayedgard@gmail.com>
2015-08-03 21:11:41 -0500

    galeria de fotos

--- pages/multimedia.php
+++ pages/multimedia.php
@@ -3,85 +3,25 @@
 <head>
 		<title>PAQUETES TUR&Iacute;STICOS</title>
 		<meta charset="utf-8">
-		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta name="format-detection" content="telephone=no" />
 		<link rel="icon" href="../images/favicon.ico">
 		<link rel="shortcut icon" href="../images/favicon.ico" />
+		<link rel="stylesheet" href="../css/style.css">
 
-
-
-
-    
-
-
-
-
-
-		<link rel="stylesheet" href="../css/style.css">	
-		<link rel="stylesheet" href="../css/booking.css">		
-		<link rel="stylesheet" href="../css/camera.css">			
+		<link rel="stylesheet" href="../css/booking.css">		<!-- -->
+		<link rel="stylesheet" href="../css/camera.css">				<!-- -->
 		<link rel="stylesheet" href="../css/owl.carousel.css">
 
-		<script src="../js/jquery.js"></script>
-		<script src="../js/jquery-migrate-1.2.1.js"></script>
-		<script src="../js/script.js"></script>
-		<script src="../js/superfish.js"></script>
-		<script src="../js/jquery.ui.totop.js"></script>
-		<script src="../js/jquery.equalheights.js"></script>
-		<script src="../js/jquery.mobilemenu.js"></script>
-		<script src="../js/jquery.easing.1.3.js"></script>
+	
         
-          <!-- para la galeria de fotos -->
-  <link rel="stylesheet" href="../galeria/css/example.css">
-  <link rel="stylesheet" href="../galeria/css/font-awesome.min.css">
+ 
+<!--para la galeria-->
 
-  		<!-- -->
-		<script src="../js/owl.carousel.js"></script>
-		<script src="../js/camera.js"></script>
-		<script src="../js/jquery.mobile.customized.min.js"></script>	
-		<script src="../js/booking.js"></script>
+ <link rel="stylesheet" type="text/css" href="../css/jquery.lightbox.css">
 
-
-
-
-
-
-
-
-
-
-
-		<!-- -->
-
-			<script>
-		$(document).ready(function(){
-			$().UItoTop({ easingType: 'easeOutQuart' });
-		});
-		</script>
-     <!--fin galeria de fotos-->
+<!--fin galeria-->
      
-     <!----para los videos----->
-<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/swfobject/2.2/swfobject.js"></script>
-<!------fin videos--->
-
-     
-     
-		<script>
-		$(document).ready(function(){
-			$().UItoTop({ easingType: 'easeOutQuart' });
-		});
-		</script>
-		<!--[if lt IE 8]>
-		<div style=' clear: both; text-align:center; position: relative;'>
-			<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
-				<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
-			</a>
-		</div>
-		<![endif]-->
-		<!--[if lt IE 9]>
-		<script src="js/html5shiv.js"></script>
-		<link rel="stylesheet" media="screen" href="css/ie.css">
-		<![endif]-->
+    
 
 		<script type="text/javascript" src="../js/move-top.js"></script>
 <script type="text/javascript" src="../js/easing.js"></script>
@@ -112,170 +52,18 @@
 			new WOW().init();
 		</script>
 
+
+
+
+
+
+
+
+
+
 	</head>
+
 	<body>
-             
-
-
-<?php
-
-include_once("../conexion.php");
-	$link = Conectarse();
-	
-$id= $_GET['cod'];
-if($id<=0)
-{ $id=1;}	
-
-
-
-// consulta para realizar el modificado de la tabla
-$consulta = mysql_query("SELECT nombreCategoria FROM categoriafotos WHERE idCategoriaFoto='$id'",$link);
-$row = mysql_fetch_array($consulta);
-
-
-?>
-
-     <?php
-
-	//conuslta para paquetes aleatorios
-$fotos ="SELECT  nombreFoto FROM fotos WHERE idCategoriafoto='$id' ORDER BY RAND()"; 
-$rfotos = mysql_query($fotos,$link);
-	
-		//nombre del titulo
-	$nombrefoto=array();
-
-while($row2 = mysql_fetch_array($rfotos))
-		{
-		array_push($nombrefoto,$row2[0]);
-		}
-	
-
-	
-	//conuslta para las categoria de fotos
-$catego ="SELECT  idCategoriaFoto, nombreCategoria FROM categoriafotos ORDER BY RAND()"; 
-$cat = mysql_query($catego,$link);
-	
-		//nombre del titulo
-	$idcat=array();
-	$nombres=array();
-
-while($row1 = mysql_fetch_array($cat))
-		{
-		array_push($idcat,$row1[0]);	
-		array_push($nombres,$row1[1]);
-		}
-	
-
-
-
-
-
-?>
-
-<script>
-    
-    $(window).load(function() {  
-    var $container = $('#container');
-    //Run to initialise column sizes
-    updateSize();
-
-    //Load masonry when images all loaded
-    $container.imagesLoaded( function(){
-
-        $container.isotope({
-            // options
-            itemSelector : '.element',	
-            layoutMode : 'masonry',
-            transformsEnabled: true,
-            columnWidth: function( containerWidth ) {
-                containerWidth = $browserWidth;
-                return Math.floor(containerWidth / $cols);
-              }
-        });
-    });
-    
-	    // update columnWidth on window resize
-    $(window).smartresize(function(){  
-        updateSize();
-        $container.isotope( 'reLayout' );
-    });
-	
-    //Set item size
-    function updateSize() {
-        $browserWidth = $container.width();
-        $cols = 4;
-
-        if ($browserWidth >= 1170) {
-            $cols = 4;
-        }
-        else if ($browserWidth >= 800 && $browserWidth < 1170) {
-            $cols = 3;
-        }
-        else if ($browserWidth >= 480 && $browserWidth < 800) {
-            $cols = 2;
-        }
-        else if ($browserWidth >= 320 && $browserWidth < 480) {
-            $cols = 1;
-        }
-        else if ($browserWidth < 401) {
-            $cols = 1;
-        }
-        //console.log("Browser width is:" + $browserWidth);
-        //console.log("Cols is:" + $cols);
-
-        // $gutterTotal = $cols * 20;
-		$browserWidth = $browserWidth; // - $gutterTotal;
-        $itemWidth = $browserWidth / $cols;
-        $itemWidth = Math.floor($itemWidth);
-
-        $(".element").each(function(index){
-            $(this).css({"width":$itemWidth+"px"});             
-        });
-			
-
-    
-	  var $optionSets = $('#options .option-set'),
-	      $optionLinks = $optionSets.find('a');
-
-	  $optionLinks.click(function(){
-	    var $this = $(this);
-	    // don't proceed if already selected
-	    if ( $this.hasClass('selected') ) {
-	      return false;
-	    }
-	    var $optionSet = $this.parents('.option-set');
-	    $optionSet.find('.selected').removeClass('selected');
-	    $this.addClass('selected');
-
-	    // make option object dynamically, i.e. { filter: '.my-filter-class' }
-	    var options = {},
-	        key = $optionSet.attr('data-option-key'),
-	        value = $this.attr('data-option-value');
-	    // parse 'false' as false boolean
-	    value = value === 'false' ? false : value;
-	    options[ key ] = value;
-	    if ( key === 'layoutMode' && typeof changeLayoutMode === 'function' ) {
-	      // changes in layout modes need extra logic
-	      changeLayoutMode( $this, options )
-	    } else {
-	      // otherwise, apply new options
-	      $container.isotope( options );
-	    }
-	    
-	    return false;
-	  });		
-
-    };
-
-        // Initialize the gallery
-        $('.thumb').touchTouch();
-    
-    });
-    
-  </script>
-
-
-
 <!--==============================header=================================-->
 	<header class="wow bounceInRight animated" data-wow-delay="0.4s" style="visibility: visible; -webkit-animation-delay: 0.4s;" >
 			<div class="container_12">
@@ -305,87 +93,112 @@ while($row1 = mysql_fetch_array($cat))
 <!--==============================Content=================================-->
 
 
-<span class="block2" >
+<?php
 
-</span>
-		<div class="content" >
-       
-			<div class="container_12">
-
-			
-
-            <div class="row1">
-
-        		<div class="col-lg-12 col-md-12 col-sm-12">
+include_once("../conexion.php");
+	$link = Conectarse();
+	
+$id= $_GET['cod'];
+if($id<=0)
+{ $id=1;}	
 
 
 
-
-                  <div id="options" class="clearfix">
-
-                      <ul id="filters" class="pagination option-set clearfix" data-option-key="filter">
-                          <?php
-			  				for($k=0;$k<count($nombres);$k++)
-			  				{
-			   				?>
-								<li><a href="multimedia.php?cod=<?php echo $idcat[$k];?>"><?php echo $nombres[$k]; ?></a></li>
-				
-               				<?php  } ?>
-                      </ul>
-
-                  </div><!-- #options -->
+// consulta para realizar el modificado de la tabla
+$consulta = mysql_query("SELECT nombreCategoria FROM categoriafotos WHERE idCategoriaFoto='$id'",$link);
+$row = mysql_fetch_array($consulta);
 
 
 
+//conuslta para paquetes aleatorios
+$fotos ="SELECT  nombreFoto FROM fotos WHERE idCategoriafoto='$id' ORDER BY RAND()"; 
+$rfotos = mysql_query($fotos,$link);
+	
+//nombre del titulo
+$nombrefoto=array();
 
-                  <div class="containerExtra">
-                  <div id="container" class="clearfix">
-                   <h3 style="padding-top:15px"><?php echo $row[0]; ?></h3>
+while($row2 = mysql_fetch_array($rfotos))
+		{
+		array_push($nombrefoto,$row2[0]);
+		}
+	
 
-                  <?php
-                  for($j=0;$j<count($nombres);$j++)
-			  				{
-                  ?>
-                   
-                    <div class="element transition people" data-category="transition">
-                    	<a href="img/page3_bigimg1.jpg" class="thumb"><figure class="img-polaroid"><img src="../administracion/imagenes/fotos/<?php echo $row[0]; ?>/<?php echo $nombrefoto[$j]; ?>" alt=""></figure></a><span class="description">quia non numquam</span>
-                    </div>
+	
+//conuslta para las categoria de fotos
+$catego ="SELECT  idCategoriaFoto, nombreCategoria FROM categoriafotos ORDER BY RAND()"; 
+$cat = mysql_query($catego,$link);
+	
+//nombre del titulo
+$idcat=array();
+$nombres=array();
 
-                     <?php  } ?>
-
-    		       </div>
-               </div>
+while($row1 = mysql_fetch_array($cat))
+		{
+		array_push($idcat,$row1[0]);	
+		array_push($nombres,$row1[1]);
+		}
+	
+?>
 
 
 
 
-	        </div>
 
 
-            </div>
-
-					
 
 
-		    
-                        
-			   
+
+
+
+
+<div class="content">
+	<div class="container_12">
+   
+	    <div><!--para los titulos-->
+
+	    	<?php
+				for($k=0;$k<count($nombres);$k++)
+				{
+			?>
+					<a href="multimedia.php?cod=<?php echo $idcat[$k];?>"><?php echo $nombres[$k]; ?></a></li>
+			<?php  } ?>
+
+
+
+	    </div>
+
+    <!--bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb-->
+	
+    	<div id="container"><!--para las fotos-->
+    	
+
+			<ul class="gallery">
+		    	<?php
+		        for($j=0;$j<count($nombres);$j++)
+				{
+		        ?>
+		          	<li>
+		        	   	<a href="../administracion/imagenes/fotos/<?php echo $row[0]; ?>/<?php echo $nombrefoto[$j]; ?>"> <img src="../administracion/imagenes/fotos/<?php echo $row[0]; ?>/<?php echo $nombrefoto[$j]; ?>">
+		           		</a>
+		             </li>
+		         <?php  } ?>
+			</ul> 
+
+
+			<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+			<script src="../js/jquery.lightbox.js"></script>
+
+			<script>
+			  // Initiate Lightbox
+			  $(function() {
+			    $('.gallery a').lightbox(); 
+			  });
+			</script>
+
+    	</div>  
+     </di> 				
         
-          </div>
-                
-                
-                
-               
-	</div>
-           
-		
-       
-       
-
-        
-       				
-        
-        
+ </div>
         
 <!--==============================footer=================================-->
 		<footer>
